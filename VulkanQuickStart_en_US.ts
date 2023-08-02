c06533d3645f29aae95a817259eafb26cbe70d7d
btipton-ds <btipton@darkskyinnovation.com>
2023-08-02 11:13:59 +0800

    QT build is working, but not reliably.

--- VulkanQuickStart_en_US.ts
+++ VulkanQuickStart_en_US.ts
@@ -5,6 +5,7 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="132"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="132"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
@@ -12,6 +13,8 @@
     <message>
         <location filename="mainwindow.ui" line="66"/>
         <location filename="mainwindow.ui" line="73"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="136"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="137"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="136"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="137"/>
         <source>PushButton</source>
@@ -19,30 +22,35 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="103"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="138"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="138"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="115"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="139"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="139"/>
         <source>toolBar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="126"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="133"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="133"/>
         <source>New...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="131"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="134"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="134"/>
         <source>Open...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="136"/>
+        <location filename="VulkanQuickStart.exe_autogen/include/ui_mainwindow.h" line="135"/>
         <location filename="VulkanQuickStart_autogen/include/ui_mainwindow.h" line="135"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
